a4bcaed71503f770f934d09d61ce9f029b6f430b
Asen Prodanov <Asen.Prodanov@Softwaregroup-bg.com>
2017-06-01 14:25:10 +0300

    Init project

---
+++ MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace RazerDeathadderFix
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            mouseEventPrinter = new System.Windows.Forms.ListBox();
+            this.hookInvokerButton = new System.Windows.Forms.Button();
+            this.clearMouseEventPrinterButton = new System.Windows.Forms.Button();
+            this.countBypassedClicksLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mouseEventPrinter
+            // 
+            mouseEventPrinter.FormattingEnabled = true;
+            mouseEventPrinter.Location = new System.Drawing.Point(12, 80);
+            mouseEventPrinter.Name = "mouseEventPrinter";
+            mouseEventPrinter.Size = new System.Drawing.Size(365, 316);
+            mouseEventPrinter.TabIndex = 0;
+            // 
+            // hookInvokerButton
+            // 
+            this.hookInvokerButton.Location = new System.Drawing.Point(12, 12);
+            this.hookInvokerButton.Name = "hookInvokerButton";
+            this.hookInvokerButton.Size = new System.Drawing.Size(98, 23);
+            this.hookInvokerButton.TabIndex = 1;
+            this.hookInvokerButton.Text = "HOOK!";
+            this.hookInvokerButton.UseVisualStyleBackColor = true;
+            this.hookInvokerButton.Click += new System.EventHandler(this.hookInvokerButton_Click);
+            // 
+            // clearMouseEventPrinterButton
+            // 
+            this.clearMouseEventPrinterButton.Location = new System.Drawing.Point(133, 12);
+            this.clearMouseEventPrinterButton.Name = "clearMouseEventPrinterButton";
+            this.clearMouseEventPrinterButton.Size = new System.Drawing.Size(70, 23);
+            this.clearMouseEventPrinterButton.TabIndex = 5;
+            this.clearMouseEventPrinterButton.Text = "CLear";
+            this.clearMouseEventPrinterButton.UseVisualStyleBackColor = true;
+            this.clearMouseEventPrinterButton.Click += new System.EventHandler(this.clearMouseEventPrinterButton_Click);
+            // 
+            // countBypassedClicksLabel
+            // 
+            this.countBypassedClicksLabel.AutoSize = true;
+            this.countBypassedClicksLabel.Location = new System.Drawing.Point(12, 54);
+            this.countBypassedClicksLabel.Name = "countBypassedClicksLabel";
+            this.countBypassedClicksLabel.Size = new System.Drawing.Size(35, 13);
+            this.countBypassedClicksLabel.TabIndex = 6;
+            this.countBypassedClicksLabel.Text = "Bypassed clicks: NaN";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 410);
+            this.Controls.Add(this.countBypassedClicksLabel);
+            this.Controls.Add(this.clearMouseEventPrinterButton);
+            this.Controls.Add(this.hookInvokerButton);
+            this.Controls.Add(mouseEventPrinter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button hookInvokerButton;
+        private System.Windows.Forms.Button clearMouseEventPrinterButton;
+        private System.Windows.Forms.Label countBypassedClicksLabel;
+        private static System.Windows.Forms.ListBox mouseEventPrinter;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,140 @@
+﻿namespace RazerDeathadderFix
+{
+    using System;
+    using System.Windows.Forms;
+    using System.Runtime.InteropServices;
+    using System.Diagnostics;
+
+    public delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    public partial class MainForm : Form
+    {
+        private const long DifferenceBetweenEachClick = 1500000;
+
+        //Declare the hook handle as an int.
+        private static int hHook = 0;
+
+        private static readonly Stopwatch Stopwatch = new Stopwatch();
+
+        private static long LastEllapsedTime = 0;
+
+        private static long bypassedClicks = 0;
+
+        //Declare the mouse hook constant.
+        //For other hook types, you can obtain these values from Winuser.h in the Microsoft SDK.
+        public const int WH_MOUSE_LL = 14;
+
+        //This is the Import for the SetWindowsHookEx function.
+        //Use this function to install a thread-specific hook.
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hInstance, int threadId);
+
+        //This is the Import for the UnhookWindowsHookEx function.
+        //Call this function to uninstall the hook.
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool UnhookWindowsHookEx(int idHook);
+
+        //This is the Import for the CallNextHookEx function.
+        //Use this function to pass the hook information to the next hook procedure in chain.
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int CallNextHookEx(int idHook, int nCode, IntPtr wParam, IntPtr lParam);
+
+        public MainForm()
+        {
+            this.InitializeComponent();
+        }
+
+        public static IntPtr MouseHookCallBack(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode < 0)
+            {
+                return (IntPtr) CallNextHookEx(hHook, nCode, wParam, lParam);
+            }
+            
+            MouseMessages mouseEvent = (MouseMessages) wParam;
+            if (mouseEvent == MouseMessages.WM_LBUTTONDOWN)
+            {
+                long currentTime = Stopwatch.Elapsed.Ticks;
+                if (LastEllapsedTime != 0)
+                {
+                    long timeDifference = currentTime - LastEllapsedTime;
+                    if (timeDifference < DifferenceBetweenEachClick)
+                    {
+                        LastEllapsedTime = currentTime;
+                        bypassedClicks++;
+                        mouseEventPrinter.Items.Add($"{mouseEvent}  -  {timeDifference}  -  {DateTime.Now}");
+                        return (IntPtr) 1;
+                    }
+                }
+            
+                LastEllapsedTime = currentTime;
+            }
+            
+            return (IntPtr) CallNextHookEx(hHook, nCode, wParam, lParam);
+        }
+
+        private void hookInvokerButton_Click(object sender, EventArgs e)
+        {
+            if (hHook == 0)
+            {
+                hHook = SetWindowsHookEx(WH_MOUSE_LL, MouseHookCallBack, (IntPtr)0, 0);
+
+                if (hHook == 0)
+                {
+                    throw new Exception("SetWindowsHookEx Failed");
+                }
+                
+                Stopwatch.Start();
+                this.countBypassedClicksLabel.Text = $@"Bypassed clicks: {bypassedClicks}";
+                this.hookInvokerButton.Text = @"UnHook!";
+            }
+            else
+            {
+                bool ret = UnhookWindowsHookEx(hHook);
+                //If the UnhookWindowsHookEx function fails.
+                if (ret == false)
+                {
+                    throw new Exception("UnhookWindowsHookEx Failed");
+                }
+
+                hHook = 0;
+                mouseEventPrinter.Items.Add("Unhook successfully");
+                this.hookInvokerButton.Text = @"HOOK!";
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (hHook != 0)
+            {
+                bool ret = UnhookWindowsHookEx(hHook);
+
+                if (ret == false)
+                {
+                    throw new Exception("Unhook fails :(");
+                }
+            }
+        }
+
+        private enum MouseMessages
+        {
+            WM_LBUTTONDOWN = 0x0201,
+
+            WM_LBUTTONUP = 0x0202,
+
+            WM_MOUSEMOVE = 0x0200,
+
+            WM_MOUSEWHEEL = 0x020A,
+
+            WM_RBUTTONDOWN = 0x0204,
+
+            WM_RBUTTONUP = 0x0205
+
+        }
+
+        private void clearMouseEventPrinterButton_Click(object sender, EventArgs e)
+        {
+            mouseEventPrinter.Items.Clear();
+        }
+    }
+}
